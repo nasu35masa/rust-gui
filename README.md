24ef491ee1a0936a04746cbc6b826a96123b0463
nasu35masa <masaya19970729@gmail.com>
2024-04-20 11:54:38 +0900

    feat: desktopアプリで再作成

--- README.md
+++ README.md
@@ -3,7 +3,5 @@
 Run the following command in the root of the project to start the Dioxus dev server:
 
 ```bash
-dx serve --hot-reload
+dx serve --hot-reload --platform desktop
 ```
-
-- Open the browser to http://localhost:8080
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,8 @@ use log::LevelFilter;
 fn main() {
     // Init debug
     dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
-    console_error_panic_hook::set_once();
 
-    launch(App);
+    dioxus::launch(App);
 }
 
 #[component]
@@ -19,12 +18,14 @@ fn App() -> Element {
         link { rel: "stylesheet", href: "main.css" }
         img { src: "header.svg", id: "header" }
         div { id: "links",
-            a { target: "_blank", href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
-            a { target: "_blank", href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-            a { target: "_blank", href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-            a { target: "_blank", href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
-            a { target: "_blank", href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-            a { target: "_blank", href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            a { href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
+            a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+            a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+            a { href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
+            a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
+                "💫 VSCode Extension"
+            }
+            a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
         }
     }
 }
